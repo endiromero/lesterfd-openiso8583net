9acadfbf77376b10042cf4f62440c793f8168aa6
John Oxley <john.oxley@liquidtelecom.mu>
2012-02-17 13:02:35 +0200

    Added test for issue 6: Unpacking a fixed length BCD field doesn't work

---
+++ OpenIso8583Net.Tests/BcdFieldTests.cs
@@ -0,0 +1,61 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="BcdFieldTests.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   BCD Field Tests
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OpenIso8583Net.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using OpenIso8583Net.FieldValidator;
+    using OpenIso8583Net.Formatter;
+    using OpenIso8583Net.LengthFormatters;
+
+    /// <summary>
+    /// BCD Field Tests
+    /// </summary>
+    [TestClass]
+    public class BcdFieldTests
+    {
+        #region Public Methods and Operators
+
+        /// <summary>
+        /// The bcd unpack odd length field.
+        /// </summary>
+        [TestMethod]
+        public void BcdUnpackOddLengthField()
+        {
+            var fd = new FieldDescriptor(new FixedLengthFormatter(3), FieldValidators.N, Formatters.Bcd);
+            byte[] msg = { 0x01, 0x23 };
+            int newOffset;
+            var fieldValue = fd.Unpack(2, msg, 0, out newOffset);
+            Assert.AreEqual("123", fieldValue);
+            Assert.AreEqual(2, newOffset);
+        }
+
+        /// <summary>
+        /// The bcd unpack even length field.
+        /// </summary>
+        [TestMethod]
+        public void BcdUnpackEvenLengthField()
+        {
+            var fd = new FieldDescriptor(new FixedLengthFormatter(3), FieldValidators.N, Formatters.Bcd);
+            byte[] msg = { 0x01, 0x23 };
+            int newOffset;
+            var fieldValue = fd.Unpack(2, msg, 0, out newOffset);
+            Assert.AreEqual("0123", fieldValue);
+            Assert.AreEqual(2, newOffset);
+        }
+
+        #endregion
+    }
+}
